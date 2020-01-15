355bbbeae7d06f6735a92882f72ccdb1c9e616d4
taher968 <taherrampurawala5253@gmail.com>
2020-01-14 22:00:39 -0500

    change app.listen function

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ app.get("/",function(req,res) {
     res.render("index")
 });
 
-app.listen(3000,function() {
-    console.log("Listening on port 3000");
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
 
