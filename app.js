bc311cef4f773976a013f09ac5ab843707d7c313
taher968 <taherrampurawala5253@gmail.com>
2020-01-27 19:26:52 -0500

    made changes to file structure using express

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-var express = require("express");
-var app = express();
-app.set("view engine","ejs");
+//App changed
+// var express = require("express");
+// var app = express();
+// app.set("view engine","ejs");
 
-app.get("/",function(req,res) {
-    res.render("index")
-});
+// app.get("/",function(req,res) {
+//     res.render("index")
+// });
+
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//     console.log(`Our app is running on port ${ PORT }`);
+// });
+
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.use(express.static(path.join(__dirname,'public')));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
-});
-
+    console.log(`The App is listening on port ${PORT}`);
+})
